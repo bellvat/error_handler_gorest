0620622dd84d62fbf7b4f0de69e8346169fd02de
bellvat <msirathanarun@gmail.com>
2022-02-23 16:22:02 +0700

    git init

---
+++ lib/error_handler_gorest.rb
@@ -0,0 +1,6 @@
+require "error_handler_gorest/version"
+
+module ErrorHandlerGorest
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/error_handler_gorest/version.rb
@@ -0,0 +1,3 @@
+module ErrorHandlerGorest
+  VERSION = "0.1.0"
+end
